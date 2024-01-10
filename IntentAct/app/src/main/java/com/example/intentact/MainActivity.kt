14c28cbb9de56e58556ba33d7d1e36094715d348
David Diamanta <dantadiamanta@gmail.com>
2024-01-11 00:08:23 +0700

    explicit intent

--- IntentAct/app/src/main/java/com/example/intentact/MainActivity.kt
+++ IntentAct/app/src/main/java/com/example/intentact/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.intentact
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.btn_move_activity -> {0
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
